e7973962c7bda9fd9e17f3eda7dc679e4d4d4202
shilpa817 <budigishilpa709@gmail.com>
2025-12-27 15:21:51 +0530

    Add files via upload

---
+++ linearecursion.java
@@ -0,0 +1,16 @@
+class linearecursion
+{
+	static int Fact(int n)
+	{
+		if(n==0)
+  {
+   return 1;
+  }
+  return n*Fact(n-1);  
+ }
+ public static void main(String args[])
+ {
+  System.out.println(Fact(5));
+ }
+}
+
